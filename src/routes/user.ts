84604f93755ed3b3b0207c5e5f18661b8a20cc96
jabin-v <jabinv03@gmail.com>
2024-03-07 13:46:19 +0530

    create user endpoints

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -8,20 +8,47 @@ import { UserService } from "../services/UserService";
 import logger from "../config/logger";
 import { User } from "../entity/User";
 import { AppDataSource } from "../config/data-source";
+import createUserValidator from "../validators/create-user-validator";
+import updateUserValidator from "../validators/update-user-validator";
+import { UpdateUserRequest } from "../types";
 
 const router = express.Router();
 const userRepository = AppDataSource.getRepository(User);
 const userService = new UserService(userRepository);
-
 const userController = new UserController(userService, logger);
 
 router.post(
     "/",
     authenticate,
     canAccess([Roles.ADMIN]),
+    createUserValidator,
     async (req: Request, res: Response, next: NextFunction) => {
         await userController.create(req, res, next);
     },
+);
+
+router.patch(
+    "/:id",
+    authenticate,
+    canAccess([Roles.ADMIN]),
+    updateUserValidator,
+    (req: UpdateUserRequest, res: Response, next: NextFunction) =>
+        userController.update(req, res, next),
+);
+
+router.get("/", authenticate, canAccess([Roles.ADMIN]), (req, res, next) =>
+    userController.getAll(req, res, next),
+);
+
+router.get("/:id", authenticate, canAccess([Roles.ADMIN]), (req, res, next) =>
+    userController.getOne(req, res, next),
+);
+
+router.delete(
+    "/:id",
+    authenticate,
+    canAccess([Roles.ADMIN]),
+    (req, res, next) => userController.destroy(req, res, next),
 );
 
 export default router;
